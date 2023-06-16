038acbbd8154c2e0e717eea51b4e63f05e694c45
TCCARAR <TCCARAR@TC18051555.turkcell.entp.tgc>
2023-06-16 14:14:31 +0300

    first commit

---
+++ polymorphismDemo/src/polymorphismDemo/EmailLogger.java
@@ -0,0 +1,9 @@
+package polymorphismDemo;
+
+public class EmailLogger extends BaseLogger{
+	
+	@Override
+	public void log(String message) {
+		System.out.println("logged to email " + message);
+	}
+}
